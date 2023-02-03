dda890936e5ac74ee1afd42a2d8e2ba3cf543cac
Casey Deccio <casey@deccio.net>
2023-02-03 10:45:04 -0700

    iterative change

--- 2023-02-03/spin.c
+++ 2023-02-03/spin.c
@@ -3,6 +3,7 @@
 #include<unistd.h>
 #include<signal.h>
 #include<sys/types.h>
+#include <sys/wait.h>
 
 void myspecialfunc(int signum) {
 	printf("just kidding %d\n", signum);
@@ -11,6 +12,8 @@ void myspecialfunc(int signum) {
 int main(int argc, char *argv[]) {
 	int i;
 
+	sigset_t new_set, old_set;
+
 	// setup signal handler
 	struct sigaction sigact;
 	sigact.sa_handler = myspecialfunc;
@@ -18,7 +21,14 @@ int main(int argc, char *argv[]) {
 
 	sigaction(SIGINT, &sigact, NULL);
 
+	sigemptyset(&new_set);
+	sigaddset(&new_set, SIGINT);
+	sigprocmask(SIG_BLOCK, &new_set, &old_set);
+
 	for (i = 0; i < 1000; i++) {
+		if (i == 10) {
+			sigprocmask(SIG_SETMASK, &old_set, NULL);
+		}
 		printf("main loop %d\n", i);
 		sleep(1);
 	}
