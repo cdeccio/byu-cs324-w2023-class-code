c2a60e5b61694ebe60c183b00b454f2cfb5f3295
Casey Deccio <casey@deccio.net>
2023-02-08 10:36:25 -0700

    iterative change

--- 2023-02-08/test.py
+++ 2023-02-08/test.py
@@ -4,7 +4,9 @@ import subprocess
 import time
 
 def main():
-    print('hello world')
+    time.sleep(1)
+    p = subprocess.Popen(['/bin/cat', 'foo'])
+    time.sleep(2)
 
 if __name__ == '__main__':
     main()
