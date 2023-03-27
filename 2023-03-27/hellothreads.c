fedcf05580c955b92d11f7d985f988b3c6b10bf4
Casey Deccio <casey@deccio.net>
2023-03-27 10:47:56 -0600

    iterative change

--- 2023-03-27/hellothreads.c
+++ 2023-03-27/hellothreads.c
@@ -4,8 +4,7 @@
 
 
 int main() {
-#pragma omp parallel for
-	//schedule(static, 1)
+#pragma omp parallel for schedule(static, 1)
 	for (int i = 0; i < 12; i++) {
 		printf("hello world iter=%d thread=%d\n", i,
 				omp_get_thread_num());
