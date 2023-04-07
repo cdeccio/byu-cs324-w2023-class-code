b9422adf9c1ac0f6d05a27e62305dcd206f61c71
Casey Deccio <casey@deccio.net>
2023-04-07 09:52:26 -0600

    iterative change

--- 2023-04-07/test.c
+++ 2023-04-07/test.c
@@ -4,7 +4,10 @@
 
 int main() {
 	int i = 0;
-	printf("x: %x\n", &i);
-	printf("m: %x\n", main);
-	printf("d: %ld\n", (unsigned long int)&i - (unsigned long int)main);
+	if (fork() == 0) {
+		i++;
+	} else {
+		i--;
+	}
+	printf("x: %x %d\n", &i, i);
 }
